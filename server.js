c21fdf17c82fcf93b91b37be404fbe1c4c093019
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2023-01-26 00:05:56 +0700

    init server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+// set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// run when  clients connect
+io.on("connection", (socket) => {
+  console.log("new ws connection..");
+});
 
 const PORT = 8000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
