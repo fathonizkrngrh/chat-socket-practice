024495997aabd215ffd30d59fea602fdf04f27a2
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2023-01-26 00:23:17 +0700

    add conection

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(express.static(path.join(__dirname, "public")));
 // run when  clients connect
 io.on("connection", (socket) => {
   console.log("new ws connection..");
+
+  socket.emit("message", "welcome to chatexpress");
+
+  // broadcast when user join
+  socket.broadcast.emit("message", " a user has joined the chat");
+
+  // when client disconnect
+  socket.on("disconnect", () => {
+    io.emit("message", "a user has left the chat");
+  });
 });
 
 const PORT = 8000 || process.env.PORT;
